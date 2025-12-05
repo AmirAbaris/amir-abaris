db1e9f846eefbea90d34995c5554bca60f2189db
Amir Abaris <thisisamirabaris@gmail.com>
2025-12-06 02:34:18 +0330

    refactor: remove projects section from homepage to streamline layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { ProfileSection } from "@/components/profile-section";
 import { ExperienceSection } from "@/components/experience-section";
-import { ProjectsSection } from "@/components/projects-section";
 import { SkillsSection } from "@/components/skills-section";
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
         <div className="relative md:w-1/2 md:py-6">
           <div className="absolute left-4 md:left-0 h-[calc(100%-2rem)] w-px bg-border top-3 md:my-6" />
           <ExperienceSection />
-          <ProjectsSection />
           <SkillsSection />
         </div>
       </div>
